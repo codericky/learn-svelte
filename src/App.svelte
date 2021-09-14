67430f1de8e170981d8b07de316311b94892d5a2
codericky <thevenom425@gmail.com>
2021-09-14 19:44:26 +0700

    edit homepage

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<h1>Hello{name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello my name, is {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">website new using svelte</p>
 	<p>Visit the <a href="https://rickynoviansyah.netlify.app">Personal website</a> about me</p>
 </main>
 
